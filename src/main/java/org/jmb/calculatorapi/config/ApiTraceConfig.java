e85aa2ff25de447bb4f424c7e46c32cf3f1b6ded
Juan Manuel Barea Martinez <jmbarea@atsistemas.com>
2020-06-25 20:33:34 +0200

    Removes unused import

--- src/main/java/org/jmb/calculatorapi/config/ApiTraceConfig.java
+++ src/main/java/org/jmb/calculatorapi/config/ApiTraceConfig.java
@@ -3,7 +3,6 @@ package org.jmb.calculatorapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.corp.calculator.TracerAPI;
 import io.corp.calculator.TracerImpl;
 
 @Configuration
