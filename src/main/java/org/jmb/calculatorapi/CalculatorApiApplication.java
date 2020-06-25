d78284e0705d77abce3afc6caac48ae63709e02f
Juan Manuel Barea Martinez <jmbarea@atsistemas.com>
2020-06-25 18:25:26 +0200

    Adds initial code

---
+++ src/main/java/org/jmb/calculatorapi/CalculatorApiApplication.java
@@ -0,0 +1,13 @@
+package org.jmb.calculatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jmb/calculatorapi/CalculatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jmb.calculatorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
